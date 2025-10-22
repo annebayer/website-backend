1d64489904e38137a1734d9b411d44049084b509
uvlwv <uvlwv@student.kit.edu>
2025-10-22 23:06:26 +0200

    start with GraphQL

--- backend/src/api/tage/controllers/tage.ts
+++ backend/src/api/tage/controllers/tage.ts
@@ -2,6 +2,48 @@
  * tage controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::tage.tage');
+export default factories.createCoreController('api::tage.tage', ({ strapi }) => ({
+  async find(ctx) {
+    const entries = await strapi.db.query('api::tage.tage').findMany({
+      populate: getPopulateObject(),
+    });
+    return entries;
+  },
+
+  async findOne(ctx) {
+    const { id } = ctx.params;
+    const entry = await strapi.db.query('api::tage.tage').findOne({
+      where: { id: Number(id) },
+      populate: getPopulateObject(),
+    });
+    return entry;
+  },
+}));
+
+/**
+ * Hilfsfunktion: Gibt das vollständige Populate-Objekt zurück
+ */
+function getPopulateObject() {
+  return {
+    TeaserBild: true, // Media-Feld auf Top-Level
+    description: true, // Blocks
+    pictures: {       // Dynamiczone
+      populate: {
+        'shared.ausfluege': {    // Component "Ausfluege"
+          populate: {
+            Bilder: {             // Component "Bilder mit Text"
+              populate: { Bilder: true }
+            },
+            tip: {                // Component "Tip"
+              populate: { Bild: true }
+            }
+          }
+        },
+        'shared.bilder-mit-text': { populate: { Bilder: true } }, // Direkt verwendete Bilder-mit-Text
+        'shared.tip': { populate: { Bild: true } }                 // Direkt verwendete Tip-Komponente
+      }
+    }
+  };
+}
