67281b9722b39708c12f62cf3ff0f164fa54c02a
annebayer <108078827+annebayer@users.noreply.github.com>
2026-02-06 20:41:44 +1300

    Refactor plugins.js to use function for exports

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,5 +1,5 @@
-module.exports = {
-  'graphql': {
+module.exports = ({ env }) => ({
+  graphql: {
     enabled: true,
     config: {
       endpoint: '/graphql',
@@ -10,6 +10,21 @@ module.exports = {
       apolloServer: {
         tracing: false,
       },
-    }
+    },
   },
-};
+
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        accessKeyId: env("R2_ACCESS_KEY_ID"),
+        secretAccessKey: env("R2_SECRET_ACCESS_KEY"),
+        endpoint: env("R2_ENDPOINT"),
+        region: "auto",
+        params: {
+          Bucket: env("R2_BUCKET"),
+        },
+      },
+    },
+  },
+});
