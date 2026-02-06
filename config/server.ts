b8ec04d30ef16c26475fa4f85385ef6d9a2e63e1
annebayer <108078827+annebayer@users.noreply.github.com>
2026-02-06 20:48:10 +1300

    Change PUBLIC_URL to PUBLIC_MEDIA_URL in server config
    Updated the configuration to use 'PUBLIC_MEDIA_URL' instead of 'PUBLIC_URL'.

--- config/server.ts
+++ config/server.ts
@@ -1,14 +1,18 @@
 export default ({ env }) => ({
-    host: env('HOST', '0.0.0.0'),
-    port: env.int('PORT', 1337),
-    url: env('PUBLIC_URL'),
-    app: {
-        keys: env.array('APP_KEYS'),
-    },
-    proxy: {
-        koa: true,
-    },
-    webhooks: {
-        populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-    },
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+
+  url: env('PUBLIC_MEDIA_URL', env('PUBLIC_URL')),
+
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+
+  proxy: {
+    koa: true,
+  },
+
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
 });
