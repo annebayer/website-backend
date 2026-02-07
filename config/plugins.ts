4f67f644c90b9dbea57a0eca356c918a8865b68a
annebayer <108078827+annebayer@users.noreply.github.com>
2026-02-07 18:45:50 +1300

    Refactor GraphQL and upload configuration structure

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,32 +1,29 @@
 export default ({ env }) => ({
-  return {
-    graphql: {
-      enabled: true,
-      config: {
-        endpoint: '/graphql',
-        shadowCRUD: true,
-        playgroundAlways: false,
-        depthLimit: 9,
-        amountLimit: 100,
-        apolloServer: {
-          tracing: false,
-        },
+  graphql: {
+    enabled: true,
+    config: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      playgroundAlways: false,
+      depthLimit: 9,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
       },
     },
-
-    upload: {
-      provider: 'aws-s3',
-      providerOptions: {
-        accessKeyId: env('R2_ACCESS_KEY_ID'),
-        secretAccessKey: env('R2_SECRET_ACCESS_KEY'),
-        endpoint: env('R2_ENDPOINT'),
-        region: 'auto',
-        forcePathStyle: true,
-        params: {
-          Bucket: env('R2_BUCKET'),
-        },
+  },
+  upload: {
+    provider: 'aws-s3',
+    providerOptions: {
+      accessKeyId: env('R2_ACCESS_KEY_ID'),
+      secretAccessKey: env('R2_SECRET_ACCESS_KEY'),
+      endpoint: env('R2_ENDPOINT'),
+      region: 'auto',
+      forcePathStyle: true,
+      params: {
+        Bucket: env('R2_BUCKET'),
       },
     },
-  };    
+  },
 });
 
