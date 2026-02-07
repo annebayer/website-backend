179cbd3ab65c3a79bc44d81b7830963c72c983ef
annebayer <108078827+annebayer@users.noreply.github.com>
2026-02-07 18:48:31 +1300

    Update plugins.ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,29 +1,36 @@
-export default ({ env }) => ({
-  graphql: {
-    enabled: true,
-    config: {
-      endpoint: '/graphql',
-      shadowCRUD: true,
-      playgroundAlways: false,
-      depthLimit: 9,
-      amountLimit: 100,
-      apolloServer: {
-        tracing: false,
-      },
-    },
-  },
-  upload: {
-    provider: 'aws-s3',
-    providerOptions: {
-      accessKeyId: env('R2_ACCESS_KEY_ID'),
-      secretAccessKey: env('R2_SECRET_ACCESS_KEY'),
-      endpoint: env('R2_ENDPOINT'),
-      region: 'auto',
-      forcePathStyle: true,
-      params: {
-        Bucket: env('R2_BUCKET'),
-      },
-    },
-  },
-});
+export default ({ env }) => {
+  console.log('UPLOAD PROVIDER CONFIG:', {
+    endpoint: env('R2_ENDPOINT'),
+    bucket: env('R2_BUCKET'),
+  });
 
+  return {
+    graphql: {
+      enabled: true,
+      config: {
+        endpoint: '/graphql',
+        shadowCRUD: true,
+        playgroundAlways: false,
+        depthLimit: 9,
+        amountLimit: 100,
+        apolloServer: {
+          tracing: false,
+        },
+      },
+    },
+
+    upload: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('R2_ACCESS_KEY_ID'),
+        secretAccessKey: env('R2_SECRET_ACCESS_KEY'),
+        endpoint: env('R2_ENDPOINT'),
+        region: 'auto',
+        forcePathStyle: true,
+        params: {
+          Bucket: env('R2_BUCKET'),
+        },
+      },
+    },
+  };
+};
