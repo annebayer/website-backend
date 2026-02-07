6a0852f280776e47cb8d261a38a0780df31e994b
ToroonEX <michigiek@t-online.de>
2026-02-08 10:05:51 +1300

    try fix wrong provider

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,40 +1,38 @@
-export default ({ env }) => {
-  console.log('UPLOAD PROVIDER CONFIG:', {
-    endpoint: env('R2_ENDPOINT'),
-    bucket: env('R2_BUCKET'),
-  });
+export default ({env}) => {
+    return {
+        graphql: {
+            enabled: true,
+            config: {
+                endpoint: '/graphql',
+                shadowCRUD: true,
+                playgroundAlways: false,
+                depthLimit: 9,
+                amountLimit: 100,
+                apolloServer: {
+                    tracing: false,
+                },
+            },
+        },
 
-  return {
-    graphql: {
-      enabled: true,
-      config: {
-        endpoint: '/graphql',
-        shadowCRUD: true,
-        playgroundAlways: false,
-        depthLimit: 9,
-        amountLimit: 100,
-        apolloServer: {
-          tracing: false,
+        upload: {
+            config: {
+                provider: "strapi-provider-cloudflare-r2",
+                providerOptions: {
+                    accessKeyId: env("R2_ACCESS_KEY_ID"),
+                    secretAccessKey: env("R2_SECRET_ACCESS_KEY"),
+                    endpoint: env("R2_ENDPOINT"),
+                    params: {
+                        Bucket: env("R2_BUCKET"),
+                    },
+                    pool: false,
+                },
+                actionOptions: {
+                    upload: {},
+                    uploadStream: {},
+                    delete: {},
+                },
+            },
         },
-      },
-    },
 
-    upload: {
-      provider: 'aws-s3',
-      providerOptions: {
-        s3Options: {
-          credentials: {
-            accessKeyId: env('R2_ACCESS_KEY_ID'),
-            secretAccessKey: env('R2_SECRET_ACCESS_KEY'),
-          },
-          endpoint: env('R2_ENDPOINT'),
-          region: 'auto',
-          forcePathStyle: true,
-        },
-        params: {
-          Bucket: env('R2_BUCKET'),
-        },
-      },
-    },
-  };
+    };
 };
