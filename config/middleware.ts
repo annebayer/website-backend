8f4300593ae46ea6579375b072edfb8ac839ac5b
ToroonEX <michigiek@t-online.de>
2026-02-08 18:46:43 +1300

    michi 9

--- config/middleware.ts
+++ config/middleware.ts
@@ -1,34 +1,39 @@
-export default ({ env }) => [
-    // ...
-    {
-        name: 'strapi::security',
-        config: {
-            contentSecurityPolicy: {
-                useDefaults: true,
-                directives: {
-                    'connect-src': ["'self'", 'https:'],
-                    'img-src': [
-                        "'self'",
-                        'data:',
-                        'blob:',
-                        'market-assets.strapi.io',
-                        env('CF_PUBLIC_ACCESS_URL')
-                            ? env('CF_PUBLIC_ACCESS_URL').replace(/^https?:\/\//, '')
-                            : '',
-                    ],
-                    'media-src': [
-                        "'self'",
-                        'data:',
-                        'blob:',
-                        'market-assets.strapi.io',
-                        env('CF_PUBLIC_ACCESS_URL')
-                            ? env('CF_PUBLIC_ACCESS_URL').replace(/^https?:\/\//, '')
-                            : '',
-                    ],
-                    upgradeInsecureRequests: null,
+export default ({ env }) => {
+    const publicUrl = env('CF_PUBLIC_ACCESS_URL');
+    const publicHost = publicUrl
+        ? publicUrl.replace(/^https?:\/\//, '')
+        : null;
+
+    return [
+        // ...
+        {
+            name: 'strapi::security',
+            config: {
+                contentSecurityPolicy: {
+                    useDefaults: true,
+                    directives: {
+                        'connect-src': ["'self'", 'https:'],
+                        'img-src': [
+                            "'self'",
+                            'data:',
+                            'blob:',
+                            'market-assets.strapi.io',
+                            ...(publicHost ? [publicHost] : []),
+                            ...(publicUrl ? [publicUrl] : []),
+                        ],
+                        'media-src': [
+                            "'self'",
+                            'data:',
+                            'blob:',
+                            'market-assets.strapi.io',
+                            ...(publicHost ? [publicHost] : []),
+                            ...(publicUrl ? [publicUrl] : []),
+                        ],
+                        upgradeInsecureRequests: null,
+                    },
                 },
             },
         },
-    },
-    // ...
-];
+        // ...
+    ];
+};
